91ce026795d0929e4afc0d88261dc9c101af1400
vatsal <vatsalsinghkv@gmail.com>
2023-01-03 17:14:50 +0530

    fix: skill icon tooltip visibility

--- components/SkillIcon.tsx
+++ components/SkillIcon.tsx
@@ -7,12 +7,12 @@ type Props = {
 
 const SkillIcon = ({ src, name }: Props) => {
   return (
-    <div className="text-2xl relative bg-white dark:bg-slate-800 shadow-md rounded-full h-14 w-14 sm:h-16 sm:w-16 grid place-items-center group">
+    <div className="text-2xl relative bg-bg-secondary shadow-md rounded-full h-14 w-14 sm:h-16 sm:w-16 grid place-items-center group">
       <Icon icon={src} width="32" height="32" />
-      <div className="opacity-0 w-max group-hover:opacity-100 absolute -top-8 rounded capitalize bg-slate-700 text-white py-1 px-2 text-sm duration-200">
+      <div className="opacity-0 invisible w-max group-hover:opacity-100 group-hover:visible absolute -top-8 rounded capitalize bg-slate-900/75 dark:bg-slate-700/90 text-white dark:text-slate-100 py-1 px-2 text-sm duration-200">
         {name}
         <svg
-          className="absolute text-slate-700 h-2 w-full left-0 top-full"
+          className="absolute text-slate-900/75 dark:text-slate-700/90 h-2 w-full left-0 top-full"
           x="0px"
           y="0px"
           viewBox="0 0 255 255"
