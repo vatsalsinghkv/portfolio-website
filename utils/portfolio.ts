fefb743173ee5537272c33f878814084a452260c
vatsal <vatsalsinghkv@gmail.com>
2023-02-08 13:22:08 +0530

    fix: dark mode img

--- utils/portfolio.ts
+++ utils/portfolio.ts
@@ -23,7 +23,7 @@ export const seoData = {
     'Vatsal Singh is a full-stack developer who specializes in building (and occasionally designing) exceptional visual interfaces.',
   author: author.name,
   image:
-    'https://user-images.githubusercontent.com/68834718/217466292-715d2dc5-92a5-4989-8423-6b4d225791b5.png"',
+    'https://user-images.githubusercontent.com/68834718/217467445-0b0d2d63-f8ad-4702-8474-814eb2cbb3be.png',
   url: 'https://vatsalsinghkv.vercel.app/',
   keywords: [
     'Vatsal',
