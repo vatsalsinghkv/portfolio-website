a6ca97ac5e2bb39f5420168e92ebbbd4b232a9bf
vatsal <vatsalsinghkv@gmail.com>
2023-03-16 16:44:30 +0530

    feat: tailwind breakpoint width getter

--- utils/helper.ts
+++ utils/helper.ts
@@ -1,11 +1,27 @@
-import { ProjectType } from '../types';
+import { BreakpointType, ProjectType } from '../types';
+import tailwindConfig from '../tailwind.config';
+import resolveConfig from 'tailwindcss/resolveConfig';
+
+const fullConfig = resolveConfig(tailwindConfig);
+
+/**
+ * Returns the breakpoint's width
+ * @param breakpoint - Configured breakpoints: xs sm md lg xl 2xl
+ * @returns width - Width of the breakpoint
+ */
+
+export const getBreakpointsWidth = (breakpoint: BreakpointType) => {
+  // @ts-ignore
+  return +fullConfig?.theme?.screens[breakpoint].slice(0, -2);
+  // ! Property 'sm' does not exist on type 'ResolvableTo<ScreensConfig>'
+};
 
 /**
  * Returns a unique id
  * @returns {String} Unique id format id123..
  */
 
-export const getId = (): string => `id${Math.random().toString(16).slice(2)}`;
+export const getId = () => `id${Math.random().toString(16).slice(2)}`;
 
 /**
  * Returns the projects by sorting them by year (descending)
@@ -15,4 +31,14 @@ export const getId = (): string => `id${Math.random().toString(16).slice(2)}`;
 
 export const sortByYear = (projects: ProjectType[]) => {
   return projects.sort((a, b) => b.year - a.year);
+};
+
+/**
+ * Modifies the given object by removing the given keys
+ * @param object object from which keys need to be removed
+ * @param keys array of keys which needs to be removed from the object
+ */
+
+export const removeKeys = <T>(object: T, keys: Array<keyof T>) => {
+  keys.forEach((field) => delete object[field]);
 };
