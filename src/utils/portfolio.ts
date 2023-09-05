c931457f55edfff8aaa61ec61ae0a3c285d0e3e7
vatsal <vatsalsinghkv@gmail.com>
2023-09-05 12:25:05 +0530

    add: beautiful animations project

--- src/utils/portfolio.ts
+++ src/utils/portfolio.ts
@@ -243,6 +243,15 @@ export const projectsSection: ProjectsSectionType = {
   projects: [
     {
       id: getId(),
+      name: 'beautiful animations',
+      url: 'https://beautiful-animations-kv.vercel.app/',
+      repo: 'https://github.com/vatsalsinghkv/beautiful-animations',
+      img: 'https://user-images.githubusercontent.com/68834718/265277125-da2a6e07-7cf6-411c-b55f-94e372aa6dc8.png',
+      year: 2023,
+      tags: ['CSS Animations', 'Sass'],
+    },
+    {
+      id: getId(),
       name: 'easy fix',
       url: 'https://easy-fix.vercel.app/',
       repo: 'https://github.com/vatsalsinghkv/easy-fix',
