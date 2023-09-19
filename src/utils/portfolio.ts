36ae4fb023195e62ef3d102fafbcba36db5420c7
vatsal <vatsalsinghkv@gmail.com>
2023-09-19 10:16:53 +0530

    content: experience update

--- src/utils/portfolio.ts
+++ src/utils/portfolio.ts
@@ -204,9 +204,10 @@ export const experienceSection: ExperienceSectionType = {
       started: 'july 2023',
       upto: 'present',
       tasks: [
-        'Migrated the codebase from React and Bootstrap to Next.js and TailwindCSS.',
-        'Implemented authentication with NextAuth for a SaaS Product.',
-        'Designed and built components like sidebar with multi-level links & drop-downs and detailed page.',
+        'Implemented UI components with React & Next.js, TypeScript & Tailwind CSS.',
+        'Developed and maintained design systems that separates design logic.',
+        'Understanding client needs and proposing effective solutions, which also involves strategising and planning.',
+        'Lead a cross-functional team of developers and designers in the creation of a SaaS product.',
       ],
     },
     {
