889ecc4510ec4c21f64359d83da2a387053d76a5
alexxuss1996 <alekseymelnych132@gmail.com>
2023-10-02 10:42:44 +0300

    feat: update next config

--- next.config.js
+++ next.config.js
@@ -2,12 +2,17 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: [
-      'user-images.githubusercontent.com',
-      'github-production-user-asset-6210df.s3.amazonaws.com',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'user-images.githubusercontent.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'github-production-user-asset-6210df.s3.amazonaws.com',
+        pathname: '/**',
+      },
     ],
-  },
-  experimental: {
-    appDir: false,
   },
 };
