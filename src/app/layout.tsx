7fcf8aff22713c828f0feeafe7622da489b01315
alexxuss1996 <alekseymelnych132@gmail.com>
2023-10-02 11:02:33 +0300

    feat: create root layout; add metadata

---
+++ src/app/layout.tsx
@@ -0,0 +1,87 @@
+import ThemeProvider from '@/lib/hooks/use-theme';
+import fontVariables from '@/lib/utils/fonts';
+import { seoData } from '@/lib/utils/portfolio';
+
+import Cursor from '@/components/ui/Cursor';
+
+import '../styles/globals.css';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: seoData.title,
+  authors: [
+    {
+      name: seoData.author,
+    },
+  ],
+  description: seoData.description,
+  keywords: seoData.keywords.join(','),
+  metadataBase: new URL(seoData.url),
+  alternates: {
+    canonical: seoData.url,
+  },
+  openGraph: {
+    type: 'website',
+    url: seoData.url,
+    title: seoData.title,
+    description: seoData.description,
+    images: seoData.image,
+    siteName: seoData.title,
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: seoData.title,
+    description: seoData.description,
+    images: seoData.image,
+    site: seoData.url,
+  },
+  icons: [
+    {
+      rel: 'apple-touch-icon',
+      sizes: '120x120',
+      url: '/favicons/apple-touch-icon.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '512x512',
+      url: '/favicons/android-chrome-512x512.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '192x192',
+      url: '/favicons/android-chrome-192x192.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      url: '/favicons/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      url: '/favicons/favicon-16x16.png',
+    },
+  ],
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className="scroll-smooth">
+      <head>
+        <script src="/scripts/no-flash.js" async />
+      </head>
+      <body className={`text-text bg-bg ${fontVariables}`}>
+        <Cursor className="hidden dark:lg:block" />
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
+    </html>
+  );
+}
