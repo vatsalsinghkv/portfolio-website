cf2bc593dd3ec5c0b840c434dbf3a290f5b0173c
vatsal <vatsalsinghkv@gmail.com>
2025-01-09 10:47:54 +0530

    refact: imports improve

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,36 +1,18 @@
 'use client';
-import Button from './buttons/Button';
-import DarkModeButton from './buttons/DarkModeButton';
-import Link from './buttons/Link';
-import NavButton from './buttons/NavButton';
-import ListItem from './lists/ListItem';
-import TabList from './lists/TabList';
-import Skill from './skills/Skill';
-import SkillIcon from './skills/SkillIcon';
-import SocialIcon from './socials/SocialIcon';
-import SocialLink from './socials/SocialLink';
-import AuthorImage from './ui/AuthorImage';
-import Cursor from './ui/Cursor';
-import ProjectCard from './ui/ProjectCard';
-import ShowLottie from './ui/ShowLottie';
-import Sidebar from './ui/Sidebar';
-import Wrapper from './ui/Wrapper';
 
-export {
-  AuthorImage,
-  Button,
-  Cursor,
-  DarkModeButton,
-  Link,
-  ListItem,
-  NavButton,
-  ProjectCard,
-  ShowLottie,
-  Sidebar,
-  Skill,
-  SkillIcon,
-  SocialIcon,
-  SocialLink,
-  TabList,
-  Wrapper,
-};
+export { default as Button } from './buttons/Button';
+export { default as DarkModeButton } from './buttons/DarkModeButton';
+export { default as Link } from './buttons/Link';
+export { default as NavButton } from './buttons/NavButton';
+export { default as ListItem } from './lists/ListItem';
+export { default as TabList } from './lists/TabList';
+export { default as Skill } from './skills/Skill';
+export { default as SkillIcon } from './skills/SkillIcon';
+export { default as SocialIcon } from './socials/SocialIcon';
+export { default as SocialLink } from './socials/SocialLink';
+export { default as AuthorImage } from './ui/AuthorImage';
+export { default as Cursor } from './ui/Cursor';
+export { default as ProjectCard } from './ui/ProjectCard';
+export { default as ShowLottie } from './ui/ShowLottie';
+export { default as Sidebar } from './ui/Sidebar';
+export { default as Wrapper } from './ui/Wrapper';
--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,29 +1,13 @@
-import About from './About';
-import Contact from './Contact';
-import Email from './Email';
-import Experience from './Experience';
-import FeaturedProjects from './FeaturedProjects';
-import Hero from './Hero';
-import Footer from './layout/Footer';
-import Layout from './layout/Layout';
-import Navbar from './layout/Navbar';
-import Projects from './Projects';
-import Skills from './Skills';
-import Social from './Social';
-import SocialLinks from './Social/SocialLinks';
-
-export {
-  About,
-  Contact,
-  Email,
-  Experience,
-  FeaturedProjects,
-  Footer,
-  Hero,
-  Layout,
-  Navbar,
-  Projects,
-  Skills,
-  Social,
-  SocialLinks,
-};
+export { default as About } from './About';
+export { default as Contact } from './Contact';
+export { default as Email } from './Email';
+export { default as Experience } from './Experience';
+export { default as FeaturedProjects } from './FeaturedProjects';
+export { default as Hero } from './Hero';
+export { default as Footer } from './layout/Footer';
+export { default as Layout } from './layout/Layout';
+export { default as Navbar } from './layout/Navbar';
+export { default as Projects } from './Projects';
+export { default as Skills } from './Skills';
+export { default as Social } from './Social';
+export { default as SocialLinks } from './Social/SocialLinks';
