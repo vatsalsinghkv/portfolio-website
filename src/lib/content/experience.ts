f07e9ea11cff8cd0e41eaddcf6f20e53a109f09a
vatsal <vatsalsinghkv@gmail.com>
2025-05-14 11:13:10 +0530

    content: dashseller experience add

--- src/lib/content/experience.ts
+++ src/lib/content/experience.ts
@@ -4,6 +4,17 @@ export const experienceSection: ExperienceSectionType = {
   title: "where i've worked",
   experiences: [
     {
+      company: 'Dashseller',
+      companyUrl: 'https://www.dashseller.com/',
+      role: 'full-stack developer',
+      started: 'sep 2024',
+      upto: 'nov 2024',
+      tasks: [
+        'Implemented development tools, GitHub Actions, workflows, and Git flow to seamless collaboration.',
+        'Integrated Notion as a CMS to manage and display content such as blogs, job postings, and documentation.',
+      ],
+    },
+    {
       company: 'Pixelwand',
       companyUrl: 'https://www.pixelwand.live/',
       role: 'frontend developer',
